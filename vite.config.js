fc72b9ee73600ddb66001911c8e477a8a36d41e3
sheharyarkhan <mshahryarkghouri@gmail.com>
2025-03-03 18:03:00 +0500

    base added

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Firebase-Signup-login-react-vite/',
   server: {
     port: 3000,
   },
